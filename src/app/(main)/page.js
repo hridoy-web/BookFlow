7d8c81a76928662e76cdc215096600149dd07be4
Hridoy Chowdhury <t.okay383@gmail.com>
2026-04-30 13:51:27 +0600

    Featured Books section added

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,25 @@
 import Banner from "@/components/Home/Banner";
+import BookCard from "@/components/Ui/BookCard";
 
-const HomePage = () => {
+const HomePage = async () => {
+
+  const res = await fetch('https://book-flow-seven.vercel.app/booksData.json')
+  const allBooksData = await res.json()
+  const topBooks = allBooksData.slice(0, 4)
+
   return (
     <div>
       <Banner />
+
+      <section className="">
+        <h2 className="text-2xl md:text-3xl font-bold mb-4 md:mb-6">Featured Books</h2>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-5 mb-12 ">
+          {
+            topBooks.map(books => <BookCard key={books.id} books={books} />)
+          }
+        </div>
+      </section>
+
     </div>
   );
 };
---
+++ src/components/Ui/BookCard.js
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import React from 'react';
+
+const BookCard = ({books}) => {
+
+    const {image_url, author, category, title } = books;
+
+    return (
+        <div className="card bg-base-100 shadow-md border border-gray-200">
+      <figure className="h-48 overflow-hidden">
+        <Image
+        src={image_url}
+        alt={title}
+        width={340}
+        height={200}
+        />
+      </figure>
+      <div className="card-body p-4">
+        <h2 className="card-title text-lg font-bold">{title}</h2>
+        <p className="text-sm opacity-70">By {author}</p>
+        <div className="badge badge-secondary badge-outline">{category}</div>
+        <div className="card-actions mt-4">
+          <button className="btn font-bold w-full">View Details</button>
+        </div>
+      </div>
+    </div>
+    );
+};
+
+export default BookCard;
